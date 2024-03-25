b0cb9a89379036f754bb22918b770b2c42da2f15
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-26 02:05:03 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240326/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240326/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -10,20 +10,21 @@ di = [-1, 0, 1, 0]
 dj = [0, -1, 0, 1]
 
 graph[i][j] = 1
-j += 1
-graph[i][j] = 2
+if n != 1:
+    j += 1
+    graph[i][j] = 2
 
-while cnt != n*n-2:
-    li = i + di[(dr+1)%4]
-    lj = j + dj[(dr+1)%4]
-    if graph[li][lj] == 0:
-        dr = (dr+1)%4
-    else:
-        cnt += 1
-        ni = i + di[dr]
-        nj = j + dj[dr]
-        graph[ni][nj] = graph[i][j] + 1
-        i, j = ni, nj
+    while cnt != n*n-2:
+        li = i + di[(dr+1)%4]
+        lj = j + dj[(dr+1)%4]
+        if graph[li][lj] == 0:
+            dr = (dr+1)%4
+        else:
+            cnt += 1
+            ni = i + di[dr]
+            nj = j + dj[dr]
+            graph[ni][nj] = graph[i][j] + 1
+            i, j = ni, nj
 
 for row in graph:
     print(*row)
