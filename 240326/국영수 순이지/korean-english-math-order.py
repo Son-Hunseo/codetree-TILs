40f9c708a155ce62a1f18e4fce280424ab9d4162
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-26 17:41:59 +0900

    Title: 국영수 순이지, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/국영수 순이지/korean-english-math-order.py
+++ 240326/국영수 순이지/korean-english-math-order.py
@@ -4,7 +4,7 @@ data = []
 for _ in range(n):
     data.append(tuple(map(str, input().split())))
 
-data.sort(key=lambda x:(x[1], x[2], x[3]), reverse=True)
+data.sort(key=lambda x:(-int(x[1]), -int(x[2]), -int(x[3])))
 
 for name, guk, eng, math in data:
     print(name, guk, eng, math)
