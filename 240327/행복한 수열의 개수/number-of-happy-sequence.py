2f998c94a6b943971e86a5b2447dc980ec4fb557
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-27 16:23:37 +0900

    Title: 행복한 수열의 개수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,37 @@
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+def check(idx, method):
+    if method == "row":
+        cnt = 1
+        bf_num = 0
+        for i in range(n):
+            if graph[idx][i] == bf_num:
+                cnt += 1
+                if cnt == m:
+                    return 1
+            else:
+                bf_num = graph[idx][i]
+                cnt = 1
+    else:
+        cnt = 1
+        bf_num = 0
+        for i in range(n):
+            if graph[i][idx] == bf_num:
+                cnt += 1
+                if cnt == m:
+                    return 1
+            else:
+                bf_num = graph[i][idx]
+                cnt = 1
+    return 0
+
+result = 0
+for i in range(n):
+    result += check(i, "row")
+    result += check(i, "col")
+
+print(result)
