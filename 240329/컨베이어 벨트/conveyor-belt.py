b8df47ab8a4e9209a6898e6edc253866fa5a15fb
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-29 00:34:14 +0900

    Title: 컨베이어 벨트, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240329/컨베이어 벨트/conveyor-belt.py
+++ 240329/컨베이어 벨트/conveyor-belt.py
@@ -1,21 +1,22 @@
 n, t = map(int, input().split())
 
-graph = []
-for _ in range(2):
-    graph.append(list(map(int, input().split())))
+graph = list(map(int, input().split()))
+graph += list(map(int, input().split()))
 
 def moving():
-    temp1 = graph[0][n-1]
-    temp2 = graph[1][0]
-    for i in range(n-1, 0, -1):
-        graph[0][i] = graph[0][i-1]
-    for j in range(0, n-1):
-        graph[1][j] = graph[1][j+1]
-    graph[0][0] = temp2
-    graph[1][n-1] = temp1
+    temp = graph[2*n-1]
+    for i in range(len(graph)-1, 0, -1):
+        graph[i] = graph[i-1]
+    graph[0] = temp
 
 for _ in range(t):
     moving()
 
-for row in graph:
-    print(*row)
+for i in range(n):
+    if i != n-1:
+        print(graph[i], end=" ")
+    else:
+        print(graph[i])
+
+for i in range(n, 2*n):
+    print(graph[i], end=" ")
