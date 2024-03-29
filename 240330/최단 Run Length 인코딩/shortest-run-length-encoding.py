85f20f90611d5cf270a24c867f31f9fa6ecc3201
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-30 01:55:19 +0900

    Title: 최단 Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240330/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,25 @@
+data = list(input())
+
+def encoding(data):
+    ndata = []
+    cur = data[0]
+    cnt = 1
+    for i in range(1, len(data)):
+        if data[i] == cur:
+            cnt += 1
+        else:
+            ndata.append(cur)
+            ndata.append(cnt)
+            cnt = 1
+            cur = data[i]
+        if i == len(data)-1:
+            ndata.append(cur)
+            ndata.append(cnt)
+    return ndata
+
+result = []
+for i in range(1, len(data)):
+    data = [data[-1]] + data[:len(data)-1]
+    result.append(len(encoding(data)))
+
+print(min(result))
