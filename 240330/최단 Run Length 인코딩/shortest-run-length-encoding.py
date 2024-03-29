a535b0e70064b02a3eabfc1bd4f33edd8ffbacfc
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-30 01:59:11 +0900

    Title: 최단 Run Length 인코딩, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240330/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240330/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -18,7 +18,7 @@ def encoding(data):
     return ndata
 
 result = []
-for i in range(1, len(data)):
+for _ in range(len(data)):
     data = [data[-1]] + data[:len(data)-1]
     result.append(len(encoding(data)))
 
