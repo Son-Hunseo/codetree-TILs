ed3636705d58d2110fbf7571cfcb3211ae4eafc5
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-03-30 02:12:27 +0900

    Title: 최단 Run Length 인코딩, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240330/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240330/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,28 +1,28 @@
 data = list(input())
 
 def encoding(data):
-    ndata = []
-    cur = data[0]
-    cnt = 1
-    for i in range(1, len(data)):
-        if data[i] == cur:
-            cnt += 1
-        else:
-            ndata.append(cur)
-            ndata.append(cnt)
-            cnt = 1
-            cur = data[i]
-        if i == len(data)-1:
-            ndata.append(cur)
-            ndata.append(cnt)
-    return ndata
+    if len(data) != 1:
+        ndata = []
+        cur = data[0]
+        cnt = 1
+        for i in range(1, len(data)):
+            if data[i] == cur:
+                cnt += 1
+            else:
+                ndata.append(cur)
+                ndata.append(cnt)
+                cnt = 1
+                cur = data[i]
+            if i == len(data)-1:
+                ndata.append(cur)
+                ndata.append(cnt)
+        return ndata
+    else:
+        return [data[0], 1]
 
 result = []
 for _ in range(len(data)):
     data = [data[-1]] + data[:len(data)-1]
     result.append(len(encoding(data)))
 
-if result:
-    print(min(result))
-else:
-    print(2)
+print(min(result))
