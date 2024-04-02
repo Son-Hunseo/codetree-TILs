cc9396c330c4b0e61c2703333fd9f86562993d24
Son <66631831+Son-Hunseo@users.noreply.github.com>
2024-04-02 12:45:44 +0900

    Title: 대폭발, Time: 190ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240402/대폭발/big-explosion.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m, r, c = map(int, input().split())
+r, c = r-1, c-1
+
+graph = [[0 for _ in range(n)] for _ in range(n)]
+
+di = [-1, 0, 1, 0]
+dj = [0, -1, 0, 1]
+
+q = deque()
+
+def bomb(r, c, time):
+    # print(r, c, time)
+    global graph
+    if time == m+1:
+        return
+    graph[r][c] = 1
+    for dr in range(4):
+        nr = r + di[dr]*(2**(time-1))
+        nc = c + dj[dr]*(2**(time-1))
+        if nr < 0 or nr > n-1 or nc < 0 or nc > n-1 or graph[nr][nc] == 1:
+            continue
+        q.append((nr, nc, time+1))
+        graph[nr][nc] = 1
+    
+    while q:
+        nr, nc, nt = q.popleft()
+        bomb(nr, nc, nt)
+    bomb(r, c, time+1)
+
+bomb(r, c, 1)
+
+result = 0
+for row in graph:
+    result += sum(row)
+
+print(result)
