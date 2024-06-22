c6e42436c25eef8cc8e3cd1cb45bcfc8e2b60b31
Son Hun Seo <66631831+Son-Hunseo@users.noreply.github.com>
2024-06-22 16:00:07 +0900

    Title: 세 실수의 반올림, Time: 163ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240622/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat(), b = sc.nextFloat(), c = sc.nextFloat();
+
+        System.out.printf("%.3f\n", a);
+        System.out.printf("%.3f\n", b);
+        System.out.printf("%.3f\n", c);
+    }
+}
