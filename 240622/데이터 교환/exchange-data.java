8bc881cccf2ebafdab4e0316cf7318cba39e0dae
Son Hun Seo <66631831+Son-Hunseo@users.noreply.github.com>
2024-06-22 14:54:38 +0900

    Title: 데이터 교환, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240622/데이터 교환/exchange-data.java
+++ 240622/데이터 교환/exchange-data.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c = 7;
-        int temp = c;
+        int temp1 = b, temp2 = c;
 
         b = a;
-        c = b;
-        a = temp;
+        c = temp1;
+        a = temp2;
 
         System.out.println(a);
         System.out.println(b);
